ad4b16e34093174ba633674c440fcca98dd2a449
Joel Severin <joel.severin@icemanor.se>
2026-03-24 21:29:29 +0100

    fixup! WIP v3: wasm: Add support for Wasm (WebAssembly)
    Cleanup reboot code

--- arch/wasm/kernel/reboot.c
+++ arch/wasm/kernel/reboot.c
@@ -5,6 +5,13 @@
 #include <linux/smp.h>
 #include <linux/printk.h>
 
+static void machine_hang(void)
+{
+	unsigned int dummy = 0U;
+
+	__builtin_wasm_memory_atomic_wait32(&dummy, 0U, -1LL);
+}
+
 void machine_restart(char *cmd)
 {
 	local_irq_disable();
@@ -13,14 +20,14 @@ void machine_restart(char *cmd)
 	do_kernel_restart(cmd);
 
 	printk("Reboot failed -- System halted\n");
-	for (;;);
+	machine_hang();
 }
 
 void machine_halt(void)
 {
 	local_irq_disable();
 	smp_send_stop();
-	for (;;);
+	machine_hang();
 }
 
 void machine_power_off(void)
@@ -28,4 +35,5 @@ void machine_power_off(void)
 	local_irq_disable();
 	smp_send_stop();
 	do_kernel_power_off();
+	machine_hang();
 }
