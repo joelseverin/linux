745572e024603f4191f7818c907923ec5783f96e
Joel Severin <joel.severin@icemanor.se>
2026-03-28 17:36:36 +0100

    fixup! WIP v3: wasm: Add support for Wasm (WebAssembly)
    Only use suitable __ARCH_WANT_ macros.

--- arch/wasm/include/uapi/asm/unistd.h
+++ arch/wasm/include/uapi/asm/unistd.h
@@ -1,6 +1,16 @@
 /* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 
+#include <asm/bitsperlong.h>
+
 #define __ARCH_WANT_SYS_CLONE
-#define __ARCH_WANT_SYS_CLONE3
+#define __ARCH_WANT_SET_GET_RLIMIT
+
+#if __BITS_PER_LONG == 64
+#define __ARCH_WANT_NEW_STAT
+#else
+#define __ARCH_WANT_STAT64
+#endif
 
 #include <asm-generic/unistd.h>
+
+#define NR_syscalls	__NR_syscalls
